5d6e5fa17620ef99b26a323bbe9d9c380dfe3887
tlpackus <tlpackus@gmail.com>
2021-05-09 22:27:35 -0700

    add code to pass constructor test

--- PierresTracker/Models/Vendor.cs
+++ PierresTracker/Models/Vendor.cs
@@ -3,16 +3,16 @@ namespace PierresTracker.Models
 {
   public class Vendor
   {
-    // private static List<Vendor> _instances = new List<Vendor> {};
+    private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
-    // public int Id { get; }
-    // public List<Order> Orders { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
     public Vendor(string vendorName)
     {
-      // Name = vendorName;
-      // _instances.Add(this);
-      // Id = _instances.Count;
-      // Orders = new List<Order>{};
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
     }
     // public static void ClearAll() => _instances.Clear();
   }
