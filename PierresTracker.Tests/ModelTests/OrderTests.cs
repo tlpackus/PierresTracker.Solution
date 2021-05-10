2043380b71f9802e4649c023c9133ca397407a2d
tlpackus <tlpackus@gmail.com>
2021-05-09 21:48:41 -0700

    add boilerplate code

---
+++ PierresTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PierresTracker.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    // [TestMethod]
+    // public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    // {
+    //   Order newOrder = new Order();
+    //   Assert.AreEqual(typeof(Order), newOrder.GetType());
+    // }
+  }
+} 
---
+++ PierresTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PierresTracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    // [TestMethod]
+    // public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    // {
+    //   Vendor newVendor = new Vendor();
+    //   Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    // }
+  }
+}  
