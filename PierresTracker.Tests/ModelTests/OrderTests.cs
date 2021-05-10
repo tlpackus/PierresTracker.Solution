95fc71c250bd3988ce6ae5e7bb40573388116e76
tlpackus <tlpackus@gmail.com>
2021-05-09 21:50:35 -0700

    first passing autopassing test

--- PierresTracker.Tests/ModelTests/OrderTests.cs
+++ PierresTracker.Tests/ModelTests/OrderTests.cs
@@ -8,11 +8,11 @@ namespace PierresTracker.Tests
   [TestClass]
   public class OrderTests
   {
-    // [TestMethod]
-    // public void OrderConstructor_CreatesInstanceOfOrder_Order()
-    // {
-    //   Order newOrder = new Order();
-    //   Assert.AreEqual(typeof(Order), newOrder.GetType());
-    // }
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
   }
 } 
--- PierresTracker.Tests/ModelTests/VendorTests.cs
+++ PierresTracker.Tests/ModelTests/VendorTests.cs
@@ -8,11 +8,11 @@ namespace PierresTracker.Tests
   [TestClass]
   public class VendorTests
   {
-    // [TestMethod]
-    // public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
-    // {
-    //   Vendor newVendor = new Vendor();
-    //   Assert.AreEqual(typeof(Vendor), newVendor.GetType());
-    // }
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor();
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
   }
 }  
